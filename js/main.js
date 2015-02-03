31be327c32fa48985c48794578c1ac248b65ec00
WorkPC <baksofill@mail.ru>
2015-02-03 17:08:18 +0200

    some JS logic

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('js файл ПОДКЛЮЧЕН');
+
+
+$(document).ready(function(){
+
+
+    $.when($.getJSON('./js/errors_en.json'), $.getJSON('./js/game_en.json'), $.getJSON('./js/messagesCore_ru.json'))
+        .done(function (dataErrors, dataGame, dataCommon) {
+            var messege = {};
+
+            messege = $.extend(true, dataErrors[0], dataGame[0],  dataCommon[0]);
+
+            console.log(messege);
+
+        });
+
+
+
+});
+

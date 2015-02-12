ebf78b34f9af82eb81617c68ef894e4f4c5815ca
HomePC <baksofill@mail.ru>
2015-02-12 21:50:58 +0200

    Nika: первая заливка

--- js/main.js
+++ js/main.js
@@ -16,42 +16,42 @@ $(document).ready(function(){
     /*
      * Plugin intialization
      */
-
-    $(window).load(function () {
-        $('.loader').delay(1000).fadeOut(600);
-
-    });
-
-
-
-
-
-    $('#pagepiling').pagepiling({
-        menu: '#menu',
-        anchors: ['main', 'programs', 'artistes', 'blog'],
-        sectionsColor: ['#000', 'green', '#ee005a', '#39C'],
-        navigation: {
-            'position': 'right',
-            'tooltips': ['Винницкий Театр Огня', 'Программы', 'Артисты', 'Блог']
-        },
-        afterRender: function(){
-            $('#pp-nav').addClass('custom');
-        },
-        afterLoad: function(anchorLink, index){
-            //using index
-            if (index == 0) {
-                //$('.loader').hide(1000);
-                //console.error("Section 3 ended loading");
-            }
-
-            //using anchorLink
-            if (anchorLink == 'main') {
-                //console.error("Section 2 ended loading");
-            }
-        }
-
-    });
-
+    //
+    //$(window).load(function () {
+    //    $('.loader').delay(1000).fadeOut(600);
+    //
+    //});
+    //
+    //
+    //
+    //
+    //
+    //$('#pagepiling').pagepiling({
+    //    menu: '#menu',
+    //    anchors: ['main', 'programs', 'artistes', 'blog'],
+    //    sectionsColor: ['#000', 'green', '#ee005a', '#39C'],
+    //    navigation: {
+    //        'position': 'right',
+    //        'tooltips': ['Винницкий Театр Огня', 'Программы', 'Артисты', 'Блог']
+    //    },
+    //    afterRender: function(){
+    //        $('#pp-nav').addClass('custom');
+    //    },
+    //    afterLoad: function(anchorLink, index){
+    //        //using index
+    //        if (index == 0) {
+    //            //$('.loader').hide(1000);
+    //            //console.error("Section 3 ended loading");
+    //        }
+    //
+    //        //using anchorLink
+    //        if (anchorLink == 'main') {
+    //            //console.error("Section 2 ended loading");
+    //        }
+    //    }
+    //
+    //});
+    //
 
 });
 

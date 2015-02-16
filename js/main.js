af7d125b15808f1f0c3775d83cccb295f304f2f0
HomePC <baksofill@mail.ru>
2015-02-16 20:30:04 +0200

    AA:  reverting fale commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function(){
 
 
@@ -16,42 +14,49 @@ $(document).ready(function(){
     /*
      * Plugin intialization
      */
-    //
-    //$(window).load(function () {
-    //    $('.loader').delay(1000).fadeOut(600);
-    //
-    //});
-    //
-    //
-    //
-    //
-    //
-    //$('#pagepiling').pagepiling({
-    //    menu: '#menu',
-    //    anchors: ['main', 'programs', 'artistes', 'blog'],
-    //    sectionsColor: ['#000', 'green', '#ee005a', '#39C'],
-    //    navigation: {
-    //        'position': 'right',
-    //        'tooltips': ['Винницкий Театр Огня', 'Программы', 'Артисты', 'Блог']
-    //    },
-    //    afterRender: function(){
-    //        $('#pp-nav').addClass('custom');
-    //    },
-    //    afterLoad: function(anchorLink, index){
-    //        //using index
-    //        if (index == 0) {
-    //            //$('.loader').hide(1000);
-    //            //console.error("Section 3 ended loading");
-    //        }
-    //
-    //        //using anchorLink
-    //        if (anchorLink == 'main') {
-    //            //console.error("Section 2 ended loading");
-    //        }
-    //    }
-    //
-    //});
-    //
+
+    $(window).load(function () {
+        $('.loader').delay(1000).fadeOut(600);
+
+    });
+
+
+    $('.bxslider').bxSlider({
+        mode: 'horizontal',
+        useCSS: false,
+        //infiniteLoop: false,
+        //hideControlOnEnd: true,
+        easing: 'easeOutElastic',
+        speed: 2000
+    });
+
+
+    $('#pagepiling').pagepiling({
+        menu: '#menu',
+        anchors: ['main', 'programs', 'artistes', 'blog'],
+        sectionsColor: ['#000', 'green', '#ee005a', '#39C'],
+        navigation: {
+            'position': 'right',
+            'tooltips': ['Винницкий Театр Огня', 'Программы', 'Артисты', 'Блог']
+        },
+        afterRender: function () {
+            $('#pp-nav').addClass('custom');
+        },
+        afterLoad: function (anchorLink, index) {
+            //using index
+            if (index == 0) {
+                //$('.loader').hide(1000);
+                //console.error("Section 3 ended loading");
+            }
+
+            //using anchorLink
+            if (anchorLink == 'main') {
+                //console.error("Section 2 ended loading");
+            }
+        }
+
+    });
+
 
 });
 
